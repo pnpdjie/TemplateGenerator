847a0306190c11813039ae28b40df72fbbd72fc4
yaosha <yaosha912@qq.com>
2017-06-13 16:22:56 +0800

    first

--- src/main/java/com/onap/template/controller/LauncherController.java
+++ src/main/java/com/onap/template/controller/LauncherController.java
@@ -1,8 +1,21 @@
 package com.onap.template.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class LauncherController {
-  
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class LauncherController  implements Initializable{
+  private @FXML Button horizFirstButton;
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    // TODO Auto-generated method stub
+    
+  }
 }
